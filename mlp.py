ceabf8da14218fcbb11028b13870211337a754b1
Faris Khattak <fariskhattak8@gmail.com>
2025-02-21 00:41:13 -0600

    create forward, backward for MLP class

--- mlp.py
+++ mlp.py
@@ -182,3 +182,76 @@ class Layer:
         return dL_dW, dL_db
 
 
+class MultilayerPerceptron:
+    def __init__(self, layers: Tuple[Layer]):
+        """
+        Create a multilayer perceptron (densely connected multilayer neural network)
+        :param layers: list or Tuple of layers
+        """
+        self.layers = layers
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """
+        This takes the network input and computes the network output (forward propagation)
+        :param x: network input
+        :return: network output
+        """
+        for layer in self.layers:
+            x = layer.forward(x)
+
+        return x
+
+    def backward(self, loss_grad: np.ndarray, input_data: np.ndarray) -> Tuple[list, list]:
+        """
+        Applies backpropagation to compute the gradients of the weights and biases for all layers in the network
+        :param loss_grad: gradient of the loss function
+        :param input_data: network's input data
+        :return: (List of weight gradients for all layers, List of bias gradients for all layers)
+        """
+        dl_dw_all = []
+        dl_db_all = []
+
+        # Init delta as the loss gradient, which is dL_dY_hat
+        delta = loss_grad
+
+        # Iterate backwards through the layers
+        for i in reversed(range(len(self.layers))):
+            layer = self.layers[i]
+
+            # Get input to the current layer
+            if i == 0:
+                h = input_data
+            else:
+                h = self.layers[i-1].activations
+
+            # Call backward prop for the layer
+            dL_dW, dL_db = layer.backward(h, delta)
+
+            # Store gradients
+            dl_dw_all.insert(0, dL_dW)
+            dl_db_all.insert(0, dL_db)
+
+            # Update delta for next layer
+            delta = layer.delta
+
+        return dl_dw_all, dl_db_all
+
+
+    def train(self, train_x: np.ndarray, train_y: np.ndarray, val_x: np.ndarray, val_y: np.ndarray, loss_func: LossFunction, learning_rate: float=1E-3, batch_size: int=16, epochs: int=32) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Train the multilayer perceptron
+
+        :param train_x: full training set input of shape (n x d) n = number of samples, d = number of features
+        :param train_y: full training set output of shape (n x q) n = number of samples, q = number of outputs per sample
+        :param val_x: full validation set input
+        :param val_y: full validation set output
+        :param loss_func: instance of a LossFunction
+        :param learning_rate: learning rate for parameter updates
+        :param batch_size: size of each batch
+        :param epochs: number of epochs
+        :return:
+        """
+        training_losses = None
+        validation_losses = None
+
+        return training_losses, validation_losses
