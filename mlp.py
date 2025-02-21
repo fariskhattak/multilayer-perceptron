b87bf8c6efe869e7ee8493dd5bea5b891c03bbeb
Faris Khattak <fariskhattak8@gmail.com>
2025-02-21 00:40:24 -0600

    add softmax, squared error, cross entropy

--- mlp.py
+++ mlp.py
@@ -72,9 +72,18 @@ class Softmax(ActivationFunction):
         return exp_x / np.sum(exp_x, axis=1, keepdims=True)
 
     def derivative(self, x: np.ndarray) -> np.ndarray:
-        # Basic formula, may need to use jacobian matrix for multiple classes
         softmax_x = self.forward(x)
-        return softmax_x * (1 - softmax_x)
+
+        # Compute diagonal matrix diag(S)
+        diag_matrix = np.einsum('bi,ij->bij', softmax_x, np.eye(softmax_x.shape[1]))
+
+        # Compute outer product S @ S^T
+        outer_product = np.einsum('bi,bj->bij', softmax_x, softmax_x)
+
+        # Compute Jacobian as diag(S) - S @ S^T
+        jacobian = diag_matrix - outer_product
+
+        return jacobian
 
 class Linear(ActivationFunction):
     def forward(self, x: np.ndarray) -> np.ndarray:
@@ -95,105 +104,19 @@ class LossFunction(ABC):
 
 
 class SquaredError(LossFunction):
-    pass
+    def loss(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        return 0.5 * (y_pred - y_true) ** 2
 
+    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        return y_pred - y_true
+    
 
 class CrossEntropy(LossFunction):
-    pass
+    def loss(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        y_pred = np.clip(y_pred, 1e-12, 1.0)  # Prevent division by zero
+        return - (y_true / y_pred) / len(y_true)
+
+    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        return y_pred - y_true
 
 
-class Layer:
-    def __init__(self, fan_in: int, fan_out: int, activation_function: ActivationFunction):
-        """
-        Initializes a layer of neurons
-
-        :param fan_in: number of neurons in previous (presynpatic) layer
-        :param fan_out: number of neurons in this layer
-        :param activation_function: instance of an ActivationFunction
-        """
-        self.fan_in = fan_in
-        self.fan_out = fan_out
-        self.activation_function = activation_function
-
-        # this will store the activations (forward prop)
-        self.activations = None
-        # this will store the delta term (dL_dPhi, backward prop)
-        self.delta = None
-
-        # Initialize weights and biaes
-        self.W = None  # weights
-        self.b = None  # biases
-
-    def forward(self, h: np.ndarray):
-        """
-        Computes the activations for this layer
-
-        :param h: input to layer
-        :return: layer activations
-        """
-        self.activations = None
-
-        return self.activations
-
-    def backward(self, h: np.ndarray, delta: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Apply backpropagation to this layer and return the weight and bias gradients
-
-        :param h: input to this layer
-        :param delta: delta term from layer above
-        :return: (weight gradients, bias gradients)
-        """
-        dL_dW = None
-        dL_db = None
-        self.delta = None
-        return dL_dW, dL_db
-
-
-class MultilayerPerceptron:
-    def __init__(self, layers: Tuple[Layer]):
-        """
-        Create a multilayer perceptron (densely connected multilayer neural network)
-        :param layers: list or Tuple of layers
-        """
-        self.layers = layers
-
-    def forward(self, x: np.ndarray) -> np.ndarray:
-        """
-        This takes the network input and computes the network output (forward propagation)
-        :param x: network input
-        :return: network output
-        """
-
-        return None
-
-    def backward(self, loss_grad: np.ndarray, input_data: np.ndarray) -> Tuple[list, list]:
-        """
-        Applies backpropagation to compute the gradients of the weights and biases for all layers in the network
-        :param loss_grad: gradient of the loss function
-        :param input_data: network's input data
-        :return: (List of weight gradients for all layers, List of bias gradients for all layers)
-        """
-        dl_dw_all = []
-        dl_db_all = []
-
-
-        return None, None
-
-    def train(self, train_x: np.ndarray, train_y: np.ndarray, val_x: np.ndarray, val_y: np.ndarray, loss_func: LossFunction, learning_rate: float=1E-3, batch_size: int=16, epochs: int=32) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Train the multilayer perceptron
-
-        :param train_x: full training set input of shape (n x d) n = number of samples, d = number of features
-        :param train_y: full training set output of shape (n x q) n = number of samples, q = number of outputs per sample
-        :param val_x: full validation set input
-        :param val_y: full validation set output
-        :param loss_func: instance of a LossFunction
-        :param learning_rate: learning rate for parameter updates
-        :param batch_size: size of each batch
-        :param epochs: number of epochs
-        :return:
-        """
-        training_losses = None
-        validation_losses = None
-
-        return training_losses, validation_losses
